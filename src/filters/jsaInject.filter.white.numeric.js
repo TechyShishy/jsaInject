241c9e15e2352bccc58982c58cb74a116b9ae21a
Wesley Hirsch <emperorshishire@gmail.com>
2011-06-14 15:51:14 -0400

    Added Numeric Filter

--- src/filters/jsaInject.filter.white.numeric.js
+++ src/filters/jsaInject.filter.white.numeric.js
@@ -1,3 +1,15 @@
-(function($){
+(function($, jsaInject){
+	/**
+	 * Verifies a given string contains only numerics.
+	 * NOTE: This includes ".", "-", and ",". ( Regex of allowed characters is /[0-9.,-]/ )
+	 * If you want to verify that something is an int, use {@link filter.white.int}.
+	 * To verify that something only contains numerals, use {@link filter.white.numeral}.
+	 * @memberOf jsaInject.filter.white
+	 * @param dirty String Potentially dirty string to be passed in and verified as numeric.
+	 */
+	function numeric(dirty) {
+		return dirty.replace(/[^0-9.,-]/, "");
+	}
 
-})(jQuery);
+	jsaInject.filter.white.numeric = numeric;
+})(jQuery, jQuery.jsaInject);
