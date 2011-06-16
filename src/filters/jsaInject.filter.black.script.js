e672071e9236e912a4317744254fe313e9a7e6f6
Wesley Hirsch <emperorshishire@gmail.com>
2011-06-16 15:28:22 -0400

    Added script filter.

--- src/filters/jsaInject.filter.black.script.js
+++ src/filters/jsaInject.filter.black.script.js
@@ -1,3 +1,15 @@
-(function($){
-
-})(jQuery);
+(function($, jsaInject){
+	/**
+	 * Verifies a given string does not contain any <script> tags.
+	 * To verify a given string does not contain any of the javascript event handlers, use {@link filter.black.event}
+	 * To verify that something is an int, use {@link filter.white.int}.
+	 * @memberOf jsaInject.filter.black
+	 * @param dirty String Potentially dirty string to be passed in and verified as containing no <script> tags.
+	 * @return String A cleaned string.
+	 */
+	function script(dirty) {
+		return $(dirty).not('script').appendTo('<div />').parent().html();
+	}
+	
+	jsaInject['filter']['black']['script'] = script;
+})(jQuery, jQuery.jsaInject);
